df43c8938db8879be5e1e935d74aaed6ecea5246
fatema <planttt098@gmail.com>
2026-01-21 16:45:02 +1300

    works!

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def process():
- name = input("What is your name? ")
+name = input("What is your name? ")
+while len(name)<=0:
+ name = input("Please enter your name: ")
+else:
  print("Hi", name)
- while len(name)>0:
-  print("Hi", name)
- else:
-  process()
+
